032a1db1bb822d4b83525c4538897b237cb1adad
Matt Weaver <weaver@slac.stanford.edu>
2017-12-22 15:02:37 -0800

    Add first dtiPgp3 10Gb image.  Built against head of surf, lcls-timing-core

---
+++ firmware/targets/dtiPgp3/ruckus.tcl
@@ -0,0 +1,22 @@
+############################
+# DO NOT EDIT THE CODE BELOW
+############################
+
+# Load RUCKUS environment and library
+source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+
+# Load submodules' code and constraints
+loadRuckusTcl $::env(TOP_DIR)/submodules
+# Load development repos rather than submodules
+#loadRuckusTcl /u1/weaver/amc-carrier-core/AmcCarrierCore
+#loadRuckusTcl /u1/weaver/lcls-timing-core
+#loadRuckusTcl /u1/weaver/pgp-gen4-pcie-axis/firmware/submodules/surf
+#loadRuckusTcl /u1/weaver/surf
+
+loadRuckusTcl "$::DIR_PATH/../../common/base"
+loadRuckusTcl "$::DIR_PATH/../../common/xpm"
+loadRuckusTcl "$::DIR_PATH/../../common/dti"
+
+# Load target's source code and constraints
+loadSource      -dir  "$::DIR_PATH/hdl/"
+loadConstraints -dir  "$::DIR_PATH/hdl/"
