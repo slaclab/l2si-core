aa0c032912ede4119eb00a09e3a07b6aba23cbcf
Matt Weaver <weaver@slac.stanford.edu>
2018-03-09 14:30:38 -0800

    Fixes to build hsd_dual from git clone

--- firmware/targets/hsd_dual/ruckus.tcl
+++ firmware/targets/hsd_dual/ruckus.tcl
@@ -7,14 +7,17 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load submodules' code and constraints
 #set SURF_DIR /u1/weaver/surf
-set SURF_DIR /u1/weaver/pgp-gen4-pcie-axis/firmware/submodules/surf
+##set SURF_DIR /u1/weaver/pgp-gen4-pcie-axis/firmware/submodules/surf
 #loadRuckusTcl /u1/weaver/surf
 #loadRuckusTcl "$::SURF_DIR/axi"        "quiet"
-loadRuckusTcl "${SURF_DIR}/base"       "quiet"
+##loadRuckusTcl "${SURF_DIR}/base"       "quiet"
 #loadRuckusTcl "${SURF_DIR}/devices"    "quiet"
 #loadRuckusTcl "${SURF_DIR}/ethernet"   "quiet"
 #loadRuckusTcl "${SURF_DIR}/protocols/ssi"  "quiet"
-loadRuckusTcl "${SURF_DIR}/xilinx"     "quiet"
+##loadRuckusTcl "${SURF_DIR}/xilinx"     "quiet"
+
+loadRuckusTcl $::env(TOP_DIR)/submodules/surf/base    "quiet"
+loadRuckusTcl $::env(TOP_DIR)/submodules/surf/xilinx  "quiet"
 
 #loadRuckusTcl /u1/weaver/lcls-timing-core
 loadRuckusTcl "$::DIR_PATH/../../submodules/lcls-timing-core"
