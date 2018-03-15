175e6c97fe448373c94e08bd4d2d8e16811a4388
Matt Weaver <weaver@slac.stanford.edu>
2018-03-15 00:58:50 -0700

    Updates to submodules

--- firmware/submodules/ruckus.tcl
+++ firmware/submodules/ruckus.tcl
@@ -4,5 +4,12 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 # Load ruckus files
 loadRuckusTcl "$::DIR_PATH/surf"
 loadRuckusTcl "$::DIR_PATH/lcls-timing-core"
-loadRuckusTcl "$::DIR_PATH/amc-carrier-core"
+#loadRuckusTcl "$::DIR_PATH/amc-carrier-core"
 loadRuckusTcl "$::DIR_PATH/../common/$::env(COMMON_FILE)"
+
+#
+#  Pick the pieces out of amc-carrier-core
+#
+loadSource -dir  "$::DIR_PATH/amc-carrier-core/AmcCarrierCore/core"
+loadSource -dir  "$::DIR_PATH/amc-carrier-core/AmcCarrierCore/dcp/hdl"
+loadSource -dir  "$::DIR_PATH/amc-carrier-core/AmcCarrierCore/ip"
