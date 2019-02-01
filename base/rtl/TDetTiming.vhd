113a6d9023aa9c0a9974f073c372357f001adbe7
Larry Ruckman <ruckman@slac.stanford.edu>
2019-02-01 08:15:02 -0800

    integrating wrapper for EventHeaderCache.vhd

--- base/rtl/TDetTiming.vhd
+++ base/rtl/TDetTiming.vhd
@@ -106,17 +106,6 @@ architecture mapping of TDetTiming is
    signal timingPhy      : TimingPhyType;
    signal timingBus      : TimingBusType;
 
-   signal appTimingHdr   : TimingHeaderType; -- aligned
-   signal appExptBus     : ExptBusType;      -- aligned
-   signal timingHdr      : TimingHeaderType; -- prompt
-   signal triggerBus     : ExptBusType;      -- prompt
-   signal fullOut        : slv(NPartitions-1 downto 0);
-
-   signal pdata          : XpmPartitionDataArray(NDET_G-1 downto 0);
-   signal pdataV         : slv                  (NDET_G-1 downto 0);
-   signal tdetMaster     : AxiStreamMasterArray (NDET_G-1 downto 0);
-   signal tdetSlave      : AxiStreamSlaveArray  (NDET_G-1 downto 0);
-   signal hdrOut         : EventHeaderArray     (NDET_G-1 downto 0);
 begin
 
    trigClk         <= rxOutClk;
@@ -221,108 +210,33 @@ begin
          axilWriteMaster => axilWriteMaster,
          axilWriteSlave  => axilWriteSlave );
 
-   timingHdr          <= toTimingHeader (timingBus);
-   triggerBus.message <= ExptMessageType(timingBus.extn);
-   triggerBus.valid   <= timingBus.extnValid;
-
-   
-   U_Realign : entity work.EventRealign
-     port map ( clk            => rxOutClk,
-                rst            => rxRst,
-                timingI        => timingHdr,
-                exptBusI       => triggerBus,
-                timingO        => appTimingHdr,
-                exptBusO       => appExptBus,
-                delay          => open );
-
-   GEN_DET : for i in 0 to NDET_G-1 generate
-     
-     trigBus(i).l0a   <= pdata (i).l0a;
-     trigBus(i).l0tag <= pdata (i).l0tag;
-     trigBus(i).valid <= pdataV(i);
-
-     U_HeaderCache : entity work.EventHeaderCache
-       port map ( rst             => rxRst,
-                  --  Cache Input
-                  wrclk           => rxOutClk,
-                  -- configuration
-                  enable          => tdetTiming(i).enable,
---                cacheenable     : in  sl := '1';     -- caches headers --
-                  partition       => tdetTiming(i).partition,
-                  -- event input
-                  timing_prompt   => timingHdr,
-                  expt_prompt     => triggerBus,
-                  timing_aligned  => appTimingHdr,
-                  expt_aligned    => appExptBus,
-                  -- trigger output
-                  pdata           => pdata     (i),
-                  pdataV          => pdataV    (i),
-                  -- status
-                  cntL0           => tdetStatus(i).cntL0,
-                  cntL1A          => tdetStatus(i).cntL1A,
-                  cntL1R          => tdetStatus(i).cntL1R,
-                  cntWrFifo       => tdetStatus(i).cntWrFifo,
-                  rstFifo         => open,
-                  msgDelay        => tdetStatus(i).msgDelay,
-                  cntOflow        => tdetStatus(i).cntOflow,
-                  --  Cache Output
-                  rdclk           => tdetClk,
-                  advance         => tdetSlave (i).tReady,
-                  valid           => tdetMaster(i).tValid,
-                  pmsg            => tdetMaster(i).tDest(0),
-                  cntRdFifo       => tdetStatus(i).cntRdFifo,
-                  hdrOut          => hdrOut    (i) );
-
-     tdetMaster(i).tData(8*TDET_AXIS_CONFIG_C.TDATA_BYTES_C-1 downto 0) <= toSlv(hdrOut(i));
-     tdetMaster(i).tLast  <= '1';
-     tdetMaster(i).tKeep  <= genTKeep(TDET_AXIS_CONFIG_C);
-
-     U_DeMux : entity work.AxiStreamDeMux
-       generic map ( NUM_MASTERS_G => 2 )
-       port map ( axisClk         => tdetClk,
-                  axisRst         => tdetRst,
-                  sAxisMaster     => tdetMaster     (i),
-                  sAxisSlave      => tdetSlave      (i),
-                  mAxisMasters(0) => tdetEventMaster(i),
-                  mAxisMasters(1) => tdetTransMaster(i),
-                  mAxisSlaves (0) => tdetEventSlave (i),
-                  mAxisSlaves (1) => tdetTransSlave (i) );
-     
-   end generate;
-
-   fullp : process ( tdetTiming ) is
-     variable vfull : slv(NPartitions-1 downto 0);
-   begin
-     vfull := (others=>'0');
-     for i in 0 to NDET_G-1 loop
-       if tdetTiming(i).enable='1' and tdetTiming(i).afull='1' then
-         vfull(conv_integer(tdetTiming(i).partition)) := '1';
-       end if;
-     end loop;
-     fullOut <= vfull;
-   end process fullp;
-     
-   U_TimingFb : entity work.XpmTimingFb
-     generic map ( DEBUG_G => true )
-     port map ( clk            => txUsrClk,
-                rst            => txUsrRst,
-                status         => txStatus,
-                pllReset       => rxControl.pllReset,
-                phyReset       => rxControl.reset,
-                id             => tdetTiming(0).id,
-                l1input        => (others=>XPM_L1_INPUT_INIT_C),
-                full           => fullOut,
-                phy            => timingPhy );
-
-   p_PAddr : process (rxOutClk) is
-   begin
-     if rising_edge(rxOutClk) then
-       if (triggerBus.valid = '1' and timingBus.strobe = '1') then
-         for i in 0 to NDET_G-1 loop
-           tdetStatus(i).partitionAddr <= triggerBus.message.partitionAddr;
-         end loop;
-       end if;
-     end if;
-   end process p_PAddr;
-
+   U_HeaderCache : entity work.EventHeaderCacheWrapper
+      generic map (
+         TPD_G   => TPD_G,  
+         NDET_G  => NDET_G)      
+      port map (         
+         -- Trigger Interface (rxClk domain)
+         trigBus         => trigBus,
+         -- Readout Interface (tdetClk domain)
+         tdetClk         => tdetClk,
+         tdetRst         => tdetRst,
+         tdetTiming      => tdetTiming,
+         tdetStatus      => tdetStatus,
+         -- Event stream (tdetClk domain)
+         tdetEventMaster => tdetEventMaster,
+         tdetEventSlave  => tdetEventSlave,
+         -- Transition stream (tdetClk domain)
+         tdetTransMaster => tdetTransMaster,
+         tdetTransSlave  => tdetTransSlave,
+         -- LCLS RX Timing Interface (rxClk domain)
+         rxClk           => rxOutClk,
+         rxRst           => rxRst,
+         rxControl       => rxControl,
+         timingBus       => timingBus,
+         -- LCLS RX Timing Interface (txClk domain)
+         txClk           => txUsrClk,
+         txRst           => txUsrRst,
+         txStatus        => txStatus,
+         timingPhy       => timingPhy);
+         
 end mapping;
