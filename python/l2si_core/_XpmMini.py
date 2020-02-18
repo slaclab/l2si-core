4689cab853c1f285b31d34637ba55dfc7456b453
Benjamin Reese <bareese@slac.stanford.edu>
2020-02-18 11:15:32 -0800

    Add missing register, make resets into rogue Commands, add enum for trig rate

--- python/l2si_core/_XpmMini.py
+++ python/l2si_core/_XpmMini.py
@@ -22,13 +22,21 @@ class XpmMini(pr.Device):
             mode         = 'RW',
         )) 
 
-        self.add(pr.RemoteVariable( 
+        self.add(pr.RemoteCommand( 
             name         = "TxPllReset",
             offset       = 0x04,
             bitSize      = 1,
             bitOffset    = 18,
-            mode         = 'RW',
-        )) 
+            function     = pr.RemoteCommand.toggle
+        ))
+
+        self.add(pr.RemoteCommand( 
+            name         = "RxPllReset",
+            offset       = 0x04,
+            bitSize      = 1,
+            bitOffset    = 19,
+            function     = pr.RemoteCommand.toggle
+        ))        
 
         self.add(pr.RemoteVariable( 
             name         = "Loopback",
@@ -39,20 +47,20 @@ class XpmMini(pr.Device):
             mode         = 'RW',
         )) 
 
-        self.add(pr.RemoteVariable( 
+        self.add(pr.RemoteCommand( 
             name         = "TxReset",
             offset       = 0x04,
             bitSize      = 1,
             bitOffset    = 29,
-            mode         = 'RW',
+            function     = pr.RemoteCommand.toggle
         )) 
 
-        self.add(pr.RemoteVariable( 
+        self.add(pr.RemoteCommand( 
             name         = "RxReset",
             offset       = 0x04,
             bitSize      = 1,
             bitOffset    = 30,
-            mode         = 'RW',
+            function     = pr.RemoteCommand.toggle            
         )) 
         
         self.add(pr.RemoteVariable( 
@@ -134,12 +142,12 @@ class XpmMini(pr.Device):
             mode         = 'RO',
         )) 
         
-        self.add(pr.RemoteVariable( 
+        self.add(pr.RemoteCommand( 
             name         = "Config_L0Select_Reset",
             offset       = 0x14,
             bitSize      = 1,
             bitOffset    = 0,
-            mode         = 'RW',
+            function     = pr.RemoteCommand.toggle
         )) 
         
         self.add(pr.RemoteVariable( 
@@ -148,6 +156,7 @@ class XpmMini(pr.Device):
             bitSize      = 1,
             bitOffset    = 16,
             mode         = 'RW',
+            base         = pr.Bool
         )) 
         
         self.add(pr.RemoteVariable( 
@@ -165,6 +174,14 @@ class XpmMini(pr.Device):
             bitSize      = 16,
             bitOffset    = 0,
             mode         = 'RW',
+            enum = {
+                0: '1 MHz',
+                1: '100 kHz',
+                2: '10 kHz',
+                3: '1 kHz',
+                4: '100 Hz',
+                5: '10 Hz',
+                6: '1 Hz'}
         )) 
         
         self.add(pr.RemoteVariable( 
