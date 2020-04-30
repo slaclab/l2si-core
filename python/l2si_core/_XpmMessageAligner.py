45680fd2d144630827edb9060f21a848bca4d83a
Benjamin Reese <bareese@slac.stanford.edu>
2020-04-30 16:49:57 -0700

    Fix lint

--- python/l2si_core/_XpmMessageAligner.py
+++ python/l2si_core/_XpmMessageAligner.py
@@ -9,8 +9,6 @@
 ##############################################################################
 import pyrogue as pr
 
-import l2si_core
-
 class XpmMessageAligner(pr.Device):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
