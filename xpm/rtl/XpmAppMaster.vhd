3a83e7a1f8fc41817c7c080cad045009f8cf2651
Matt Weaver <weaver@slac.stanford.edu>
2023-04-07 19:40:44 -0700

    fix

--- xpm/rtl/XpmAppMaster.vhd
+++ xpm/rtl/XpmAppMaster.vhd
@@ -319,20 +319,6 @@ begin
          din (0)          => config.l0Select.enabled,
          dout(0)          => l0Enabled );
 
-   U_L0EnDelay : entity surf.SlvDelay
-      generic map (
-         TPD_G        => TPD_G,
-         DELAY_G      => 128,
-         REG_OUTPUT_G => false,
-         WIDTH_G      => 1 )
-      port map (
-         clk              => timingClk,                 -- [in]
-         rst              => timingRst,
-         en               => timingBus.strobe,          -- [in]
-         delay            => depth_fids_7,
-         din (0)          => config.l0Select.enabled,
-         dout(0)          => l0Enabled );
-
    L0Enb: process (config) is
    begin
      config_l0Select         <= config.l0Select;
